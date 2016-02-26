8811b241f67a53b246e427a3822a8870586223b4
Saint Remy <Saint_Remy@imac-de-dani.local>
2016-02-26 03:11:13 -0600

    Initial Commit

---
+++ Reto5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reto5
+//
+//  Created by Saint Remy on 26/02/16.
+//  Copyright © 2016 Saint Remy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
